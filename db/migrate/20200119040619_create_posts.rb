8e09cd3622b447c09ea2785dc396128b850a3b9a
syooooring <syooooring@gmail.com>
2020-01-19 13:11:55 +0900

    db_migrate

--- db/migrate/20200119040619_create_posts.rb
+++ db/migrate/20200119040619_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.text :company_name
+      t.text :name
+      t.text :email
+      t.text :content
+      
       t.timestamps
     end
   end
