84fb66edfc8b5815e7b1b3d4742dac136e38e72e
syooooring <syooooring@gmail.com>
2020-01-20 20:30:04 +0900

    params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    
   end
 
   def new
     @post = Post.new
   end
 
+  private
+    def post_params
+      params.require(:post).permit(:company_name, :name, :email, :content)
+    end
 end
