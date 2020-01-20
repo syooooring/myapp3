898040347c466c6f4f3249b8321ec27173c73aec
syooooring <syooooring@gmail.com>
2020-01-20 21:34:34 +0900

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    
   end
 
-  def show
-    Post.all
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  private
-    def post_params
-      params.require(:post).permit(:company_name, :name, :email, :content)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  resources :posts, only:[:new, :show] do
+  resources :posts, only:[:show] do
     collection do
       get 'profile'
+      get 'contact'
     end
   end
 end
