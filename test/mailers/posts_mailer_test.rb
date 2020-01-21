5f0659d63ea5491868dc1a39dca88a4793e6cf52
syooooring <syooooring@gmail.com>
2020-01-21 19:34:32 +0900

    preview

--- test/mailers/posts_mailer_test.rb
+++ test/mailers/posts_mailer_test.rb
@@ -1,7 +1,8 @@
-require 'test_helper'
+# Preview all emails at http://localhost:3000/rails/mailers/posts_mailer
+class PostsMailerPreview < ActionMailer::Preview
+  def posts
+     posts = Posts.new(company_name: "株式会社テストマン", name: "侍 太郎", email: "test@test", message: "問い合わせメッセージ")
 
-class PostsMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+     PostsMailer.send_mail(posts)
+   end
 end
